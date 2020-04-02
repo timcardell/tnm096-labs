1ed4069b8c21feebce19df7f88183db3b90b29ac
Tim Cardell <timcal04.04@gmail.com>
2020-04-02 10:37:15 +0200

    added repo

---
+++ cmake-build-debug/CMakeFiles/tnm096_labs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tnm096_labs.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-relwithdebinfo/CMakeFiles/tnm096_labs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tnm096_labs.dir/main.cpp.o"
+  "tnm096_labs"
+  "tnm096_labs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tnm096_labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
