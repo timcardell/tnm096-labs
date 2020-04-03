5c2186e3a536dc831c4ec3372e63b50cc4d4da9b
Tobias Ryttlinger <tosryr@gmail.com>
2020-04-03 14:20:21 +0200

    .

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -4,20 +4,21 @@
 #include "Puzzle.h"
 
 Puzzle::Puzzle(){
-    std::vector<int> inValues{1,2,3,0,4,6,7,5,8};
+    std::vector<int> inValues{1,2,0,4,5,6,7,8,3};
 
-    for(size_t i = 0; i < 9; i++) {
+    for(int i = 0; i < 9; i++) {
         values.push_back(inValues.at(i));
         if (inValues.at(i) == 0) {
             zeroPos = i;
         }
     }
+
     h_score = MisplacedTiles(values);
     g_score = 0;
 }
 
 Puzzle::Puzzle(std::vector<int>  InBoard, int gScore){
-    for(size_t i = 0; i < 9; i++) {
+    for(int i = 0; i < 9; i++) {
         values.push_back(InBoard.at(i));
         if (InBoard.at(i) == 0) {
             zeroPos = i;
@@ -29,42 +30,58 @@ Puzzle::Puzzle(std::vector<int>  InBoard, int gScore){
 
 Puzzle Puzzle::A_Star(){
     int steps = 1;
-
+    std::vector<std::vector<int>> OldBoards;
     while(h_score != 0){
+        std::cout << OldBoards.size() <<std::endl;
         std::vector<int> PosMoves = moves();
-        Puzzle WorkInProgressPuzzle = solve(values, PosMoves,steps) ;
+        OldBoards.push_back(values);
+        Puzzle WorkInProgressPuzzle = solve(values, PosMoves,steps, OldBoards) ;
         *this =  WorkInProgressPuzzle;
         steps++;
     }
     return *this;
 }
 
-Puzzle Puzzle::solve(std::vector<int> board, std::vector<int> moves, int gScore){
+Puzzle Puzzle::solve(std::vector<int> board, std::vector<int> moves, int gScore, std::vector<std::vector<int>> OldBoards){
 
     std::vector<Puzzle> DifferentBoards ;
 
     //Create new Puzzles
-    for(size_t i = 0; i < moves.size(); i++){
+    for(int i = 0; i < moves.size(); i++){
         std::vector<int> tempValues = board;
         std::swap(tempValues.at(moves.at(i)),tempValues.at(zeroPos));
         DifferentBoards.push_back(Puzzle(tempValues, gScore));
     }
 
     std::vector<int> fScore;
+    bool visited = false;
+    std::vector<Puzzle> FilteredBoards;
     //CAlc f score
-    for(int i = 0; i < DifferentBoards.size(); i++){
-        fScore.push_back(DifferentBoards.at(i).ManhattanDistance() + DifferentBoards.at(i).g_score);
+    for(size_t i = 0; i < DifferentBoards.size(); i++){
+        for(size_t j = 0; j < OldBoards.size(); j++) {
+            if ((DifferentBoards.at(i).values == OldBoards.at(j))) {
+                visited = true;
+                break;
+            } else visited = false;
+        }
+        if(visited == false){
+            FilteredBoards.push_back(DifferentBoards.at(i));
+        }
+    }
 
+    for(int i = 0; i < FilteredBoards.size(); i++){
+        std::cout << i << std::endl;
+        fScore.push_back(FilteredBoards.at(i).ManhattanDistance() + FilteredBoards.at(i).g_score);
     }
 
     std::vector<int>::iterator it = std::find(fScore.begin(), fScore.end(), *std::min_element(fScore.begin(),fScore.end()));
     int index = std::distance(fScore.begin(), it);
 
-    //Reuturn puzzle with least f score;
+    //Return puzzle with least f score;
     std::cout << "FScore: "<< *std::min_element(fScore.begin(),fScore.end()) <<std::endl;
-    std::cout << "GScore: " << DifferentBoards.at(index).g_score <<", " << "HScore: " << DifferentBoards.at(index).h_score <<std::endl;
-    DifferentBoards.at(index).Display();
-    return DifferentBoards.at(index);
+    std::cout << "GScore: " << FilteredBoards.at(index).g_score <<", " << "HScore: " << FilteredBoards.at(index).h_score <<std::endl;
+    FilteredBoards.at(index).Display();
+    return FilteredBoards.at(index);
 }
 
 
@@ -127,6 +144,7 @@ std::vector<int> Puzzle::moves() {
             break;
         case 5 :
             posMoves = {2, 4, 8};
+            break;
         case 6:
             posMoves = {3, 7};
             break;
@@ -135,6 +153,7 @@ std::vector<int> Puzzle::moves() {
             break;
         case 8 :
             posMoves = {5, 7};
+            break;
         default:
             break;
 
--- Puzzle.h
+++ Puzzle.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <array>        // std::array
+#include <random>       // std::default_random_engine
+#include <chrono>       // std::chrono::system_clock
 #ifndef TNM096_LABS_PUZZLE_H
 #define TNM096_LABS_PUZZLE_H
 
@@ -15,7 +18,7 @@ struct Puzzle {
     Puzzle(std::vector<int>  Board, int gScore);
     virtual ~Puzzle(){};
 
-    Puzzle solve(std::vector<int> board,std::vector<int> move,int steps);
+    Puzzle solve(std::vector<int> board,std::vector<int> move,int steps, std::vector<std::vector<int>> OldBoards);
 
     Puzzle A_Star();
 
