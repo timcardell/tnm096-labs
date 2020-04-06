c77850d5b2d5d112eb9a19aaf1678ab27cc16796
Tobias Ryttlinger <tosryr@gmail.com>
2020-04-06 13:58:04 +0200

    .

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Lab")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/tnm096_labs")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
