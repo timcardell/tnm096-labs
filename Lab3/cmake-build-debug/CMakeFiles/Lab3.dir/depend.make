710d7a65c490aeef296f175a4fdb7e6987450490
Tobias Ryttlinger <tosryr@gmail.com>
2020-04-28 16:36:13 +0200

    ..

--- Lab3/cmake-build-debug/CMakeFiles/Lab3.dir/depend.make
+++ Lab3/cmake-build-debug/CMakeFiles/Lab3.dir/depend.make
@@ -1,2 +1,2 @@
-# Empty dependencies file for TNM096_lab3.
+# Empty dependencies file for Lab3.
 # This may be replaced when dependencies are built.
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -22,9 +22,8 @@ struct Clauses {
     }
 
     bool operator<(Clauses right) const {
-        return (pos < right.pos && neg < right.neg) ;
+        return (pos < right.pos && neg < right.neg);
     }
-
 
 
     std::vector<std::string> neg;
@@ -34,39 +33,44 @@ struct Clauses {
 
 //Functions
 std::vector<Clauses> Solver(std::vector<Clauses> KB);
+
 std::vector<Clauses> Incorporate(std::vector<Clauses> S, std::vector<Clauses> KB);
+
 std::vector<Clauses> Incorporate_clause(Clauses A, std::vector<Clauses> KB);
+
 Clauses resolution(Clauses A, Clauses B);
+
 std::ostream &operator<<(std::ostream &out, Clauses &c);
+
 std::vector<Clauses> removeDuplicates(std::vector<Clauses> &KB);
+
 void display(std::vector<Clauses> KB);
+
 std::vector<std::string> GetIntersection(std::vector<std::string> A, std::vector<std::string> B);
+
 std::vector<std::string> GetUnion(std::vector<std::string> A, std::vector<std::string> B);
 
 int main() {
-    srand (time(NULL));
-    Clauses A = Clauses({"sun","money"}, {"ice"});
-    Clauses B = Clauses({"money"}, {"ice","movie"});
+    srand(time(NULL));
+    Clauses A = Clauses({"sun", "money"}, {"ice"});
+    Clauses B = Clauses({"money"}, {"ice", "movie"});
     Clauses C = Clauses({"movie"}, {"money"});
-    Clauses D = Clauses({"movie","ice"}, {});
+    Clauses D = Clauses({"movie", "ice"}, {});
     Clauses E = Clauses({}, {"movie"});
 
-    std::vector<Clauses> KB = {A,C,D,E};
+    std::vector<Clauses> KB = {A, C, D, E};
     KB = Solver(KB);
-    std::cout << "Solved: " << std::endl;
-    for (Clauses S: KB) {
-        std::cout << S << std::endl;
-    }
+
     return 0;
 }
 
 
 Clauses resolution(Clauses A, Clauses B) {
 
-    std::vector<std::string> IntersectionApBn = GetIntersection(A.pos,B.neg);
-    std::vector<std::string> IntersectionAnBp = GetIntersection(A.neg,B.pos);
+    std::vector<std::string> IntersectionApBn = GetIntersection(A.pos, B.neg);
+    std::vector<std::string> IntersectionAnBp = GetIntersection(A.neg, B.pos);
 
-    if (IntersectionApBn.size() == 0 && IntersectionAnBp.size() == 0 ) {
+    if (IntersectionApBn.size() == 0 && IntersectionAnBp.size() == 0) {
         return Clauses();
     }
 
@@ -74,28 +78,27 @@ Clauses resolution(Clauses A, Clauses B) {
         int randIndex = rand() % IntersectionApBn.size();
         std::string a = IntersectionApBn.at(randIndex);
 
-        A.pos.erase(std::remove(A.pos.begin(),  A.pos.end(), a), A.pos.end());
-        B.neg.erase(std::remove(B.neg.begin(),  B.neg.end(), a), B.neg.end());
+        A.pos.erase(std::remove(A.pos.begin(), A.pos.end(), a), A.pos.end());
+        B.neg.erase(std::remove(B.neg.begin(), B.neg.end(), a), B.neg.end());
 
-    }
-    else {
+    } else {
         int randIndex = rand() % IntersectionAnBp.size();
         std::string a = IntersectionAnBp.at(randIndex);
 
-        A.neg.erase(std::remove(A.neg.begin(),  A.neg.end(), a), A.neg.end());
-        B.pos.erase(std::remove(B.pos.begin(),  B.pos.end(), a), B.pos.end());
+        A.neg.erase(std::remove(A.neg.begin(), A.neg.end(), a), A.neg.end());
+        B.pos.erase(std::remove(B.pos.begin(), B.pos.end(), a), B.pos.end());
     }
 
     Clauses C;
-    std::vector<std::string> UnionApBp = GetUnion(A.pos,B.pos);
-    std::vector<std::string> UnionAnBn = GetUnion(A.neg,B.neg);
+    std::vector<std::string> UnionApBp = GetUnion(A.pos, B.pos);
+    std::vector<std::string> UnionAnBn = GetUnion(A.neg, B.neg);
 
 
     C.pos = UnionApBp;
     C.neg = UnionAnBn;
 
-    std::vector<std::string> IntersectionCpCn = GetIntersection(C.pos,C.neg);
-    if(IntersectionCpCn.size() != 0){
+    std::vector<std::string> IntersectionCpCn = GetIntersection(C.pos, C.neg);
+    if (IntersectionCpCn.size() != 0) {
         return Clauses();
     }
 
@@ -105,7 +108,6 @@ Clauses resolution(Clauses A, Clauses B) {
     C.pos.erase(unique(C.pos.begin(), C.pos.end()), C.pos.end());
     C.neg.erase(unique(C.neg.begin(), C.neg.end()), C.neg.end());
 
-
     return C;
 
 }
@@ -113,11 +115,11 @@ Clauses resolution(Clauses A, Clauses B) {
 std::vector<Clauses> Solver(std::vector<Clauses> KB) {
 
     std::vector<Clauses> KBDot;
-    while(true) {
+    do {
         std::vector<Clauses> S = {};
         KBDot = KB;
 
-        for (int i= 0; i < KB.size()-1; i++) {
+        for (int i = 0; i < KB.size() - 1; i++) {
             for (int j = i + 1; j < KB.size(); j++) {
 
                 Clauses C = resolution(KB.at(i), KB.at(j));
@@ -126,30 +128,30 @@ std::vector<Clauses> Solver(std::vector<Clauses> KB) {
                     S.push_back(C);
                 }
             }
-
         }
         S = removeDuplicates(S);
+        if (S.size() == 0) {
 
-        if (S.size() == 0){
             return KB;
         }
 
-        std::cout <<std::endl<<"This is S in do: "<<std::endl;
+        std::cout <<"S =  " ;
         display(S);
-        std::cout <<std::endl;
+        std::cout << std::endl;
 
         //Incorporate S in KB
         KB = removeDuplicates(KB);
         KB = Incorporate(S, KB);
+        std::cout << std::endl;
 
+        std::cout << "KB =  " ;
 
-        std::cout <<"This is KB after incorporate: "<<std::endl;
         display(KB);
-    }
+        std::cout << std::endl;
+    }while(KBDot != KB);
 }
 
 std::vector<Clauses> Incorporate(std::vector<Clauses> S, std::vector<Clauses> KB) {
-
     for (auto A: S) {
         KB = Incorporate_clause(A, KB);
     }
@@ -157,33 +159,29 @@ std::vector<Clauses> Incorporate(std::vector<Clauses> S, std::vector<Clauses> KB
 }
 
 std::vector<Clauses> Incorporate_clause(Clauses A, std::vector<Clauses> KB) {
-    for (auto it = KB.begin(); it != KB.end();) {
-        Clauses AB;
-        AB.pos = GetIntersection(A.pos, it->pos);
-        AB.neg = GetIntersection(A.neg, it->neg);
 
-        if (AB == A) {
-            it = KB.erase(it);
-        }
-        else if(AB.pos == it->pos && AB.neg == it->neg) {
+    for (auto it = KB.begin(); it != KB.end();) {
+        Clauses intersectionA_B;
+        intersectionA_B.pos = GetIntersection(A.pos, it->pos);
+        intersectionA_B.neg = GetIntersection(A.neg, it->neg);
+
+        if (intersectionA_B.pos == it->pos && intersectionA_B.neg == it->neg) {
             return KB;
         }
-        else {
+
+        if (intersectionA_B == A) {
+            it = KB.erase(it);
+        } else {
             it++;
         }
     }
-
-    if (std::find(KB.begin(), KB.end(), A) == KB.end()){
-        KB.push_back(A);
-    }
-
-
+    KB.push_back(A);
     return KB;
 }
 
-std::vector<Clauses> removeDuplicates(std::vector<Clauses> &KB){
-    std::sort( KB.begin(), KB.end() );
-    KB.erase( std::unique( KB.begin(), KB.end() ), KB.end() );
+std::vector<Clauses> removeDuplicates(std::vector<Clauses> &KB) {
+    std::sort(KB.begin(), KB.end());
+    KB.erase(std::unique(KB.begin(), KB.end()), KB.end());
     return KB;
 }
 
@@ -191,49 +189,49 @@ std::ostream &operator<<(std::ostream &out, Clauses &c) {
 
     if (c.pos.size() == 0 && c.neg.size() == 0) {
         out << "false" << std::endl;
-    }
-    else {
-        out << "{ ";
+    } else {
         for (int i = 0; i < c.pos.size(); i++) {
-            out << c.pos.at(i) ;
-            if(i != c.pos.size()-1){
+            out << c.pos.at(i);
+            if (i != c.pos.size() - 1) {
                 out << " V ";
             }
         }
 
-        if(c.neg.size() > 0 && c.pos.size() > 0){
+        if (c.neg.size() > 0 && c.pos.size() > 0) {
             out << " V ";
         }
 
         for (int i = 0; i < c.neg.size(); i++) {
-            out <<"-" << c.neg.at(i) ;
-            if(i != c.neg.size()-1){
+            out << "-" << c.neg.at(i);
+            if (i != c.neg.size() - 1) {
                 out << " V ";
             }
         }
-        out << "} ";
+
     }
     return out;
 }
 
-void display(std::vector<Clauses> KB){
-    for (int i = 0; i < KB.size(); i++){
-        std::cout << KB.at(i) <<std::endl;
+void display(std::vector<Clauses> KB) {
+    std::cout<<"{";
+    for (int i = 0; i < KB.size(); i++) {
+        std::cout << KB.at(i)<< ", ";
     }
+    std::cout<<"}";
 }
 
-std::vector<std::string> GetIntersection(std::vector<std::string> A, std::vector<std::string> B){
+std::vector<std::string> GetIntersection(std::vector<std::string> A, std::vector<std::string> B) {
     std::vector<std::string> intersection;
-    std::sort(A.begin(),A.end());
-    std::sort(B.begin(),B.end());
-    std::set_intersection(A.begin(),A.end(),B.begin(),B.end(),std::back_inserter(intersection));
+    std::sort(A.begin(), A.end());
+    std::sort(B.begin(), B.end());
+    std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), std::back_inserter(intersection));
     return intersection;
 }
 
-std::vector<std::string> GetUnion(std::vector<std::string> A, std::vector<std::string> B){
+std::vector<std::string> GetUnion(std::vector<std::string> A, std::vector<std::string> B) {
     std::vector<std::string> intersection;
-    std::sort(A.begin(),A.end());
-    std::sort(B.begin(),B.end());
-    std::set_union(A.begin(),A.end(),B.begin(),B.end(),std::back_inserter(intersection));
+    std::sort(A.begin(), A.end());
+    std::sort(B.begin(), B.end());
+    std::set_union(A.begin(), A.end(), B.begin(), B.end(), std::back_inserter(intersection));
     return intersection;
 }
